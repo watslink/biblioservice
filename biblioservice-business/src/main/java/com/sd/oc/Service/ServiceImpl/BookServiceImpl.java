2eeab25eace810a4e612e18bd6505cec6246775b
watslink <seb.decroix@gmail.com>
2019-05-10 13:49:19 +0200

    ajout de BookService + BookServiceImpl + methode findById

---
+++ biblioservice-business/src/main/java/com/sd/oc/Service/ServiceImpl/BookServiceImpl.java
@@ -0,0 +1,22 @@
+package com.sd.oc.Service.ServiceImpl;
+
+import com.sd.oc.DAO.BookDAO;
+import com.sd.oc.Service.ServiceInterface.BookService;
+import com.sd.oc.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    @Autowired
+    BookDAO bookDAO;
+
+    @Override
+    public Book get(int id) {
+        Optional<Book> optBook=bookDAO.findById(id);
+        return optBook.orElse(null);
+    }
+}
---
+++ biblioservice-business/src/main/java/com/sd/oc/Service/ServiceInterface/BookService.java
@@ -0,0 +1,8 @@
+package com.sd.oc.Service.ServiceInterface;
+
+import com.sd.oc.model.Book;
+
+public interface BookService {
+
+    Book get(int id);
+}
