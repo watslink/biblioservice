cd7755f54d7236ca63582be585d8e683774242c9
watslink <seb.decroix@gmail.com>
2019-05-21 13:53:02 +0200

    ajout method getUser et checkUserConnection dans interface UserService

--- biblioservice-business/src/main/java/com/sd/oc/Service/ServiceInterface/UserService.java
+++ biblioservice-business/src/main/java/com/sd/oc/Service/ServiceInterface/UserService.java
@@ -1,4 +1,10 @@
 package com.sd.oc.Service.ServiceInterface;
 
+import com.sd.oc.model.User;
+
 public interface UserService {
+
+    User getUser(int id);
+
+    User checkUserConnection(String username, String password);
 }
--- biblioservice-consumer/src/main/java/com/sd/oc/DAO/UserDAO.java
+++ biblioservice-consumer/src/main/java/com/sd/oc/DAO/UserDAO.java
@@ -4,4 +4,6 @@ import com.sd.oc.model.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserDAO extends CrudRepository<User, Integer> {
+
+    User findByUsername(String username);
 }
